9125d909887d83b0d10a6881676150a3fc7facbf
Ahmed-Khaled-25 <62831783+Ahmed-Khaled-25@users.noreply.github.com>
2025-11-17 17:07:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,125 @@
 from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+import json
+
+
+
+
+
+######### NOTEEE #########
+# to run api run this file first main.py 
+#by writing --> uvicorn main:app in vscode terminal
+##########################
+
+
+
+
+api_keys = [
+    "akljnv13bvi2vfo0b0bmw"
+]  # This is encryption key
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")  # use token authentication
+
+
+def api_key_auth(api_key: str = Depends(oauth2_scheme)):
+    if api_key not in api_keys:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Forbidden"
+        )
+
+
+
+with open('patient_vitals_data.json') as json_file:
+    data = json.load(json_file)
+
 
 
 
 app = FastAPI()
 
-@app.get("/akljnv13bvi2vfo0b0bmx")
-async def test():
-    return "it works 199902502"
+
+
+
+
+###############################################################################
+
+
+#information like name, age ...
+
+@app.get("/patient_info", dependencies=[Depends(api_key_auth)])
+async def read_root():
+    Data = data["patient_info"]
+
+    return {f"{Data}"}
+
+
+
+###############################################################################
+
+
+#all vitals includ ECG signals, O2 and temp.
+#by passing day number and spesific hour needed the data will be sent
+
+@app.get("/patient_vitals/day/{d_num}/hour/{h_num}", dependencies=[Depends(api_key_auth)])
+async def read_root(d_num: int, h_num: int):
+    day_number = d_num - 1
+    hour_number = h_num - 1
+    Data = data["patient_vitals"][day_number][f"day_{d_num}"][hour_number][f"hour_{h_num}"]
+
+    return {f"{Data}"}
+
+
+
+###############################################################################
+
+#AI part
+
+@app.get("/Ai/Ai_response", dependencies=[Depends(api_key_auth)])
+async def reead_root():
+    return {"AI_response": "MY AI root Works"}
+
+
+@app.post("/Ai/receive_ai_text", dependencies=[Depends(api_key_auth)])
+async def receive_ai_text(text_data: dict):
+    #print(f"Response from simple endpoint: {response_explicit.json()}")
+    print(text_data)
+    return {"AI_response": "MY AI root Works"}
+
+
+
+###############################################################################
+
+#Reciving data from arduino throw PC
+@app.post("/post_test", dependencies=[Depends(api_key_auth)])
+async def receive_data(data: dict):
+    print(data)
+    return {f"data"}
+
+
+
+###############################################################################
+
+#Others reading for patient and ecg state
+@app.get("/beat_per_min", dependencies=[Depends(api_key_auth)])
+async def reead_bpm():
+    Data = data["beat_per_min"]
+    return {f"{Data}"}
+
+
+@app.get("/ECG_reading", dependencies=[Depends(api_key_auth)])
+async def reead_ecg():
+    Data = data["ECG_reading"]
+    return {f"{Data}"}
+
+
+@app.get("/ECG_state", dependencies=[Depends(api_key_auth)])
+async def reead_ecg_s():
+    Data = data["ECG_state"]
+    return {f"{Data}"}
+
+
+
+
 
 
